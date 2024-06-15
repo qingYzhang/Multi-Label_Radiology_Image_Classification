c147a0070590cb47c7bcfec5897d13fb1ea943fe
Zhang <zhangs18@bigpurple-ln1.cm.cluster>
2024-06-15 16:33:39 -0400

    adjustment

--- utils/prepare/preprocess.py
+++ utils/prepare/preprocess.py
@@ -2,11 +2,11 @@ import csv
 import json
 
 # Define the CSV file path
-csv_file_path = './Data_Entry_2017.csv'
+csv_file_path = '../../../Dataset/Chest/Data_Entry_2017.csv'
 
 # Define the path to the train list text file
-train_list_path = './train_val_list.txt'
-test_list_path = './test_list.txt'
+train_list_path = '../../../Dataset/Chest/train_val_list.txt'
+test_list_path = '../../../Dataset/Chest/test_list.txt'
 
 # Define the list of possible findings
 possible_findings = [
@@ -46,7 +46,7 @@ with open(csv_file_path, mode='r') as csv_file:
     for row in csv_reader:
         findings = row['Finding Labels'].split('|')
         target_vector = create_target_vector(findings, possible_findings)
-        img_path = f"../data/all_images/{row['Image Index']}"
+        img_path = f"../Dataset/Chest/all_images/{row['Image Index']}"
 
         if row['Image Index'] in train_list:
             data["train"].append({"target": target_vector, "img_path": img_path})
@@ -58,10 +58,10 @@ with open(csv_file_path, mode='r') as csv_file:
             data["test"].append({"target": test_target_vector, "img_path": img_path})
 
 # Save the train and test data to JSON files
-with open('train_data.json', 'w') as train_file:
+with open('../../data/chest/train_data.json', 'w') as train_file:
     json.dump(data["train"], train_file, indent=4)
 
-with open('test_data.json', 'w') as test_file:
+with open('../../data/chest/test_data.json', 'w') as test_file:
     json.dump(data["test"], test_file, indent=4)
 
 print("JSON files for training and testing data have been created successfully.")
